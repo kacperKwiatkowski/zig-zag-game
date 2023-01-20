8c86da334667c6b7809ff7b0e1ae5e67c41714b4
kacperKwiatkowski <kacper18k@gmail.com>
2023-01-20 17:12:02 +0100

    Character is moving with camera

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    void Awake()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
