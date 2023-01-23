9afeb199b3ccfed0fad655ec75556c4163d6c7d7
kacperKwiatkowski <kacper18k@gmail.com>
2023-01-23 17:19:21 +0100

    Zig zag game completed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,7 @@ public class GameManager : MonoBehaviour
     public void StartGame()
     {
         gameStarted = true;
+        FindObjectOfType<Road>().StartBuilding();
     }
 
     private void Update()
---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    public GameObject roadPrefab;
+    public float offset = 0.706f;
+    public Vector3 lastPos;
+
+
+    private int roadCount = 0;
+
+    public void StartBuilding()
+    {
+        InvokeRepeating("CreateNewRoadPart", 1f, .5f);
+    }
+    
+    public void CreateNewRoadPart()
+    {
+        Debug.Log("CCC");
+
+        Vector3 spawnPos = Vector3.zero;
+
+        float chance = Random.Range(0, 100);
+        if (chance < 50)
+        {
+            spawnPos = new Vector3(lastPos.x + offset, lastPos.y, lastPos.z + offset);
+        }
+        else
+        {
+            spawnPos = new Vector3(lastPos.x - offset, lastPos.y, lastPos.z + offset);
+        }
+
+        GameObject g = Instantiate(roadPrefab, spawnPos, Quaternion.Euler(0, 45, 0));
+        lastPos = g.transform.position;
+
+        roadCount++;
+        if (roadCount % 5 == 0)
+        {
+            g.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
